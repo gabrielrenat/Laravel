02b7180ef255583c56619f4298ff8e45fc010841
gabrielrenat <gabrielrenatovga@yahoo.com>
2022-10-19 08:49:53 -0300

    COmpleto

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $validated = $request->validate([
+            'token' => 'required|max:255',
+        ]);
+        if ($validated) {
+            try {
+                $user = User::find0rFail(Auth::user()->id);
+                $user->token = $request->get('token');
+                $user->save();
+                return $this->success($user);
+            } catch (\Throwable $th) {
+                return $this->error("Usuário não encontrado!!!", 401, $th->getMessage());
+            }
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
